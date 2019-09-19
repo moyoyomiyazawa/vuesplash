ede45e3ce3517a8f2a87ae60f20292edccb9801b
Yuya Kasai <moyoyomiyazawa@gmail.com>
2019-09-19 21:48:35 +0900

    first commit

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use App\Http\Requests\StoreComment;
+use App\Http\Requests\StorePhoto;
+use App\Photo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PhotoController extends Controller
+{
+    public function __construct()
+    {
+        // 認証が必要
+        $this->middleware('auth')->except(['index', 'show', 'download']);
+    }
+
+    /**
+     * 写真一覧
+     */
+    public function index()
+    {
+        $photos = Photo::with(['owner', 'likes'])
+            ->orderBy(Photo::CREATED_AT, 'desc')->paginate();
+
+        // モデルクラスのインスタンスをreturnすると自動的にJSONに変換される
+        return $photos;
+    }
+
+    /**
+     * 写真詳細
+     * @param string $id
+     * @return Photo
+     */
+    public function show(string $id)
+    {
+        $photo = Photo::where('id', $id)
+            ->with(['owner', 'comments.author', 'likes'])->first();
+
+        return $photo ?? abort(404);
+    }
+
+    public function download(Photo $photo)
+    {
+        // 写真の存在チェック
+        if (!Storage::cloud()->exists($photo->filename)) {
+            abort(404);
+        }
+
+        $headers = [
+            'Content-Type' => 'application/octet-stream',
+            'Content-Disposition' => 'attachment; filename="' . $photo->filename . '"',
+        ];
+
+        return response(Storage::cloud()->get($photo->filename), 200, $headers);
+    }
+
+    /**
+     * 写真投稿
+     * @param StorePhoto $request
+     * @return \Illuminate\Http\Response
+     */
+    // public function create(StorePhoto $request)
+    public function create(StorePhoto $request)
+    {
+        // 投稿写真の拡張子を取得する
+        $extension = $request->photo->extension();
+
+        $photo = new Photo();
+
+        // インスタンス生成時に割り振られたランダムなID値と本来の拡張子を組み合わせてファイル名とする
+        $photo->filename = $photo->id . '.' . $extension;
+
+        // S3にファイルを保存する
+        // 第三引数の'public'はファイルを公開状態で保存するため
+        Storage::cloud()->putFileAs('', $request->photo, $photo->filename, 'public');
+
+        // データベースエラー時にファイル削除を行うためトランザクションを利用する
+        DB::beginTransaction();
+
+        try {
+            Auth::user()->photos()->save($photo);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            // DBとの不整合を避けるためアップロードしたファイルを削除
+            Storage::cloud()->delete($photo->filename);
+            throw $exception;
+        }
+
+        // リソースの新規作成なのでレスポンスコードは201(CREATED)を返却する
+        return response($photo, 201);
+    }
+
+    /**
+     * コメント投稿
+     * @param Photo $photo
+     * @param StoreComment $request
+     * @return \Illuminate\Http\Response;
+     */
+    public function addComment(Photo $photo, StoreComment $request)
+    {
+        $comment = new Comment();
+        $comment->content = $request->get('content');
+        $comment->user_id = Auth::user()->id;
+        $photo->comments()->save($comment);
+
+        // authorリレーションをロードするためにコメントを取得しなおす
+        $new_comment = Comment::where('id', $comment->id)->with('author')->first();
+
+        return response($new_comment, 201);
+    }
+
+    /**
+     * いいね
+     * @param string $id
+     * @return array
+     */
+    public function like(string $id)
+    {
+        $photo = Photo::where('id', $id)->with('likes')->first();
+
+        if (!$photo) {
+            abort(404);
+        }
+
+        $photo->likes()->detach(Auth::user()->id);
+        $photo->likes()->attach(Auth::user()->id);
+
+        return ["photo_id" => $id];
+    }
+
+    /**
+     * いいね解除
+     * @param string $id
+     * @return array
+     */
+    public function unlike(string $id)
+    {
+        $photo = Photo::where('id', $id)->with('likes')->first();
+
+        if (!$photo) {
+            abort(404);
+        }
+
+        $photo->likes()->detach(Auth::user()->id);
+
+        return ["photo_id" => $id];
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,26 @@
+import './bootstrap'
+
+import Vue from 'vue'
+
+// ルーティングの定義をインポートする
+import router from './router'
+
+// ストアをインポートする
+import store from './store'
+
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+const createApp = async () => {
+    await store.dispatch('auth/currentUser')
+
+    new Vue({
+        el: '#app',
+        router,  // ルーティングの定義を読み込む
+        store,  // ストアを読み込む
+        components: { App },  // ルートコンポーネントの使用を宣言する
+        template: '<App />'  // ルートコンポーネントを描画する
+    })
+}
+
+createApp()
