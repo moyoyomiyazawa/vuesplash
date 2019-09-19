d3b7bdd2bdb72f64d6ffa7c6e89df53e9151e7a9
Yuya Kasai <moyoyomiyazawa@gmail.com>
2019-09-19 22:44:56 +0900

    修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production')
         {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
